d43a32d3ecf73967ca03184858b16d354d328048
lucasquearis <lucasquearis@hotmail.com>
2021-11-23 12:20:32 -0300

    refatora todo create sale

--- controllers/sales/create.js
+++ controllers/sales/create.js
@@ -1,18 +1,12 @@
-const Joi = require('joi');
 const serviceCreate = require('../../services/sales/create');
 
-const ERROR_MESSAGE = 'Wrong product ID or invalid quantity';
-
 const create = async (req, res, next) => {
-  const { error } = Joi.array().items(Joi.object({
-    productId: Joi.string().required().min(24).rule({ message: ERROR_MESSAGE }),
-    quantity: Joi.number().required().min(1).rule({ message: ERROR_MESSAGE })
-    .messages({ 'number.base': ERROR_MESSAGE }),
-  })).validate(req.body);
-  if (error) return next(error);
-  const newSale = await serviceCreate(req.body);
-  if ('err' in newSale) return next(newSale.err);
-  res.status(200).json(newSale);
+  try {
+    const newSale = await serviceCreate(req.body);
+    res.status(200).json(newSale);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = create;
--- services/sales/create.js
+++ services/sales/create.js
@@ -1,6 +1,10 @@
+const Joi = require('joi');
+
 const modelGetProductById = require('../../models/products/getById');
 const modelCreate = require('../../models/sales/create');
 const modelUpdateProduct = require('../../models/products/updateQuantity');
+
+const ERROR_MESSAGE = 'Wrong product ID or invalid quantity';
 
 const updateQuantityProduct = async (quantityVendido, quantityEstoque) => {
   const result = quantityEstoque.quantity - quantityVendido.quantity;
@@ -8,7 +12,18 @@ const updateQuantityProduct = async (quantityVendido, quantityEstoque) => {
   await modelUpdateProduct(quantityVendido.productId, result);
 };
 
+const validateParams = (body) => {
+  const { error } = Joi.array().items(Joi.object({
+    productId: Joi.string().required().min(24).rule({ message: ERROR_MESSAGE }),
+    quantity: Joi.number().required().min(1).rule({ message: ERROR_MESSAGE })
+    .messages({ 'number.base': ERROR_MESSAGE }),
+  })).validate(body);
+  return error;
+};
+
 const create = async (body) => {
+  const errorParams = validateParams(body);
+  if (errorParams) throw errorParams;
   const exist = await body.map(async (obj) => {
     const productVerify = await modelGetProductById(obj.productId);
     if (productVerify.quantity < obj.quantity) return null;
@@ -17,12 +32,11 @@ const create = async (body) => {
   });
   const promiseResolve = await Promise.all(exist);
   if (promiseResolve.includes(null)) {
-    return {
-      err: {
-        code: 'stock_problem',
-        message: 'Such amount is not permitted to sell',
-        },
-      };
+    const errorFormat = {
+      code: 'stock_problem',
+      message: 'Such amount is not permitted to sell',
+    };
+    throw errorFormat; 
   }
   return modelCreate(body);
 };
